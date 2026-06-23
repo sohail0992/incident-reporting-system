2abe4182fa17520282aec72cd681e05a53e1f337
sohail0992 <msohail.se@gmail.com>
2026-06-24 00:46:44 +0200

    Add UserRepository interface and findByEmail to UserDao

--- src/main/java/incident_reporting/UserDao.java
+++ src/main/java/incident_reporting/UserDao.java
@@ -3,7 +3,7 @@ package incident_reporting;
 import javax.persistence.EntityManager;
 import java.util.List;
 
-public class UserDao {
+public class UserDao implements UserRepository {
 
 	private EntityManager em;
 
@@ -28,5 +28,12 @@ public class UserDao {
 
 	public List<User> findAll() {
 		return em.createQuery("from User", User.class).getResultList();
+	}
+
+	public User findByEmail(String email) {
+		List<User> results = em.createQuery("from User where email = :email", User.class)
+				.setParameter("email", email)
+				.getResultList();
+		return results.isEmpty() ? null : results.get(0);
 	}
 }
