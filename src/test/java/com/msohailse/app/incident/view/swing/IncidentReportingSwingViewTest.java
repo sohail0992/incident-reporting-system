c714686292ef68e78d33b600eed80f6581e54c5d
sohail0992 <msohail.se@gmail.com>
2026-07-07 00:21:09 +0200

    failing tests for register panel

--- src/test/java/com/msohailse/app/incident/view/swing/IncidentReportingSwingViewTest.java
+++ src/test/java/com/msohailse/app/incident/view/swing/IncidentReportingSwingViewTest.java
@@ -1,9 +1,15 @@
 package com.msohailse.app.incident.view.swing;
 
+import static org.mockito.Mockito.verify;
+
+import org.assertj.swing.annotation.GUITest;
+import org.assertj.swing.core.matcher.JButtonMatcher;
+import org.assertj.swing.core.matcher.JLabelMatcher;
 import org.assertj.swing.edt.GuiActionRunner;
 import org.assertj.swing.fixture.FrameFixture;
 import org.assertj.swing.junit.runner.GUITestRunner;
 import org.assertj.swing.junit.testcase.AssertJSwingJUnitTestCase;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
@@ -43,6 +49,41 @@ public class IncidentReportingSwingViewTest extends AssertJSwingJUnitTestCase {
 	@Override
 	protected void onTearDown() throws Exception {
 		closeable.close();
+	}
+
+	// ---------------------------------------------------------------
+	// Register panel
+	// ---------------------------------------------------------------
+
+	@Test @GUITest
+	public void testRegisterPanelControlsInitialStates() {
+		GuiActionRunner.execute(() -> view.showRegisterPanel());
+		window.label(JLabelMatcher.withText("First Name").andShowing());
+		window.textBox("registerFirstNameTextBox").requireEnabled();
+		window.label(JLabelMatcher.withText("Last Name").andShowing());
+		window.textBox("registerLastNameTextBox").requireEnabled();
+		window.textBox("registerEmailTextBox").requireEnabled();
+		window.textBox("registerPasswordTextBox").requireEnabled();
+		window.button(JButtonMatcher.withText("Register")).requireDisabled();
+		window.label("registerErrorLabel").requireText(" ");
+	}
+
+	@Test
+	public void testShowErrorShouldShowMessageInRegisterErrorLabel() {
+		GuiActionRunner.execute(() -> view.showRegisterPanel());
+		GuiActionRunner.execute(() -> view.showError("Email already taken"));
+		window.label("registerErrorLabel").requireText("Email already taken");
+	}
+
+	@Test
+	public void testRegisterButtonShouldDelegateToUserControllerRegisterUser() {
+		GuiActionRunner.execute(() -> view.showRegisterPanel());
+		window.textBox("registerFirstNameTextBox").enterText("M");
+		window.textBox("registerLastNameTextBox").enterText("Sohail");
+		window.textBox("registerEmailTextBox").enterText("msohail.se@gmail.com");
+		window.textBox("registerPasswordTextBox").enterText("Password1");
+		window.button("registerButton").click();
+		verify(userController).registerUser("M", "Sohail", "msohail.se@gmail.com", "Password1");
 	}
 
 }
