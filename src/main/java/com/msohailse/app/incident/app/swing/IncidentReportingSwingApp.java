9671b002ff8c86a918dd2ec851692ff5748100dd
sohail0992 <msohail.se@gmail.com>
2026-07-08 01:05:20 +0200

    add picocli and refactor IncidentReportingSwingApp to accept DB connection CLI args, so e2e tests can point it at a Testcontainers database

--- src/main/java/com/msohailse/app/incident/app/swing/IncidentReportingSwingApp.java
+++ src/main/java/com/msohailse/app/incident/app/swing/IncidentReportingSwingApp.java
@@ -1,6 +1,9 @@
 package com.msohailse.app.incident.app.swing;
 
 import java.awt.EventQueue;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.Callable;
 
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -10,12 +13,43 @@ import com.msohailse.app.incident.controller.IncidentController;
 import com.msohailse.app.incident.controller.UserController;
 import com.msohailse.app.incident.view.swing.IncidentReportingSwingView;
 
-public class IncidentReportingSwingApp {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+
+@Command(mixinStandardHelpOptions = true)
+public class IncidentReportingSwingApp implements Callable<Void> {
+
+	@Option(names = { "--db-host" }, description = "PostgreSQL host address")
+	private String dbHost = "localhost";
+
+	@Option(names = { "--db-port" }, description = "PostgreSQL host port")
+	private int dbPort = 5432;
+
+	@Option(names = { "--db-name" }, description = "Database name")
+	private String dbName = "incident_db";
+
+	@Option(names = { "--db-user" }, description = "Database user")
+	private String dbUser = "incident_user";
+
+	@Option(names = { "--db-password" }, description = "Database password")
+	private String dbPassword = "incident_password";
 
 	public static void main(String[] args) {
+		new CommandLine(new IncidentReportingSwingApp()).execute(args);
+	}
+
+	@Override
+	public Void call() {
 		EventQueue.invokeLater(() -> {
+			Map<String, String> properties = new HashMap<>();
+			properties.put("javax.persistence.jdbc.url",
+					"jdbc:postgresql://" + dbHost + ":" + dbPort + "/" + dbName);
+			properties.put("javax.persistence.jdbc.user", dbUser);
+			properties.put("javax.persistence.jdbc.password", dbPassword);
+
 			EntityManagerFactory emf =
-				Persistence.createEntityManagerFactory("incident_reporting");
+				Persistence.createEntityManagerFactory("incident_reporting", properties);
 			JpaTransactionManager transactionManager = new JpaTransactionManager(emf);
 			IncidentReportingSwingView view = new IncidentReportingSwingView();
 			UserController userController = new UserController(transactionManager, view);
@@ -24,5 +58,6 @@ public class IncidentReportingSwingApp {
 			view.setIncidentController(incidentController);
 			view.setVisible(true);
 		});
+		return null;
 	}
 }
